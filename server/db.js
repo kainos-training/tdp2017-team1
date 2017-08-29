cc4fc195d4f3efa1f7d477daa979ee1b622f8a1f
Richard Taylor <richardta@kainos.com>
2017-08-29 14:49:28 +0100

    Added get route that gets all courses from database. created basic express skeleton complete with cors, bodyparser, morgan and error handler middleware.

--- server/db.js
+++ server/db.js
@@ -5,10 +5,20 @@ const db = mysql.createConnection({
     host: config.host,
     user: config.user,
     password: config.password,
-    database: 'world'
+    database: 'MapBooking'
 });
 
 db.connect(function(err) {
     if(err) throw err;
     console.log("Connected to MySQL");
 });
+
+exports.getCourses = function (callback){
+  db.query(
+    "SELECT * FROM courses",
+    function(err, rows) {
+      if(err) throw err;
+      callback(rows);
+    }
+  );
+};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,42 @@
+/**
+ * Module dependencies.
+ */
 const express = require ('express');
-const app = express();
+const errorHandler = require('errorhandler');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+const morgan = require('morgan');
+
 const db = require('./db.js');
 
-app.listen(8002, function () {
-	console.log('Express API listening on port 8002...')
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configuration.
+ */
+// app.use is Binding application-level middleware to an instance of the app object
+app.use(cors());
+app.use(bodyParser.urlencoded({ // for parsing application/x-www-form-urlencoded
+  extended: false
+}));
+
+// HTTP request logger middleware.
+app.use(morgan('dev'));
+// Error Handler middleware for more verbose errors
+app.use(errorHandler());
+
+/**
+ * Routes configuration.
+ */
+app.get('/getCourses', function(req, res) {
+  db.getCourses(function(rows) {
+    res.send(rows);
+  });
+});
+
+app.listen(8002, function() {
+  console.log('Express server listening on port 8002');
 });
