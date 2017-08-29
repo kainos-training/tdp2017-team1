6244a9a30dd4e4d48554b9804aff7c55a5c1d08f
Jordan Thomson <j.thomson@kainos.com>
2017-08-29 14:27:51 +0100

    variable names in course class

--- client/src/app/course.ts
+++ client/src/app/course.ts
@@ -1,5 +1,9 @@
 export class Course {
+    course_id: number;
     title: String;
-    date: Date;
+    start_date: Date;
     location: String;
+    description: String;
+    additional_info: String;
+    max_attendees: number;
 }
