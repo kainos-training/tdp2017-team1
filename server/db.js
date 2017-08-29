b9e2535c95911b53608ca871ac12ca1e89e73d3a
Curtis Tarr <30830713+tarrcurtis@users.noreply.github.com>
2017-08-29 14:15:34 +0100

    Express route for bokking course

--- server/db.js
+++ server/db.js
@@ -4,11 +4,25 @@ const config = require('./config.json');
 const db = mysql.createConnection({
     host: config.host,
     user: config.user,
-    password: config.password,
-    database: 'world'
+    //password: config.password,
+    database: 'MapBooking'
 });
 
 db.connect(function(err) {
     if(err) throw err;
     console.log("Connected to MySQL");
 });
+
+exports.bookCourse = function (attendee_name, attendee_email, course_id, callback){
+    db.query(
+        "INSERT INTO `attendees` (`attendee_name`, `attendee_email`, `course_id`) "
+        + "VALUES (?,?,?)",
+        [attendee_name, attendee_email, course_id],
+        function(err){
+            if(err) throw err;
+            callback("Course booked.");
+        }
+    );
+};
+
+
--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 const express = require ('express');
 const app = express();
 const db = require('./db.js');
+const bodyParser = require('body-parser');
 
 app.listen(8002, function () {
 	console.log('Express API listening on port 8002...')
+});
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+app.post('/book', function(req, res){
+	const attendee_name = req.body.name;
+    const attendee_email = req.body.email;
+	const course_id = req.body.course_id;
+
+    if(courseID && email && name){
+        db.bookCourse(attendee_name, attendee_email, course_id, function(message){
+            res.send(message);
+        });
+    }
 });
