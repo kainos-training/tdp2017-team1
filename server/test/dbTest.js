3a07767493cfc94d467611ea0dafb8e840249730
Richard Taylor <richardta@kainos.com>
2017-08-30 10:12:19 +0100

    Added skeleton testing framework, all tests will always pass right now

---
+++ server/test/dbTest.js
@@ -0,0 +1,14 @@
+// Lets put tests here!
+const db = require('../db.js');
+
+var assert = require('assert');
+describe('db.js', function() {
+  describe('getCourses Function', function() {
+    it('Should return empty rows object if db is empty', function() {
+      assert.ok(1)
+    });
+    it('Should return populated rows object if db has data', function() {
+      assert.ok(1)
+    });
+  });
+});
