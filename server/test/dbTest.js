7965d714e17cd86ce1e86ddf47129f2e875dfa79
Richard Taylor <richardta@kainos.com>
2017-08-30 09:55:54 +0100

    Elaborated on tests, still always pass

--- server/test/dbTest.js
+++ server/test/dbTest.js
@@ -1,10 +1,14 @@
 // Lets put tests here!
+const db = require('../db.js');
 
 var assert = require('assert');
 describe('db.js', function() {
-  describe('getCoursesRoute', function() {
-    it('Shoud Return Rows Object', function() {
-      assert.equal(1, 1);
+  describe('getCourses Function', function() {
+    it('Shoud Return Rows Object if query is valid', function() {
+      assert.ok(1)
+    });
+    it('Shoud Return error Object if query is invalid', function() {
+      assert.ok(1)
     });
   });
 });
