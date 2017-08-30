bb68d1a3b498e0cb1ddc9a6721079ebc558a1d9a
Richard Taylor <richardta@kainos.com>
2017-08-30 14:20:02 +0100

    Added skeleton calendarNotifier file

---
+++ server/calendarNotifier.js
@@ -0,0 +1,7 @@
+// This file will handle sending notifications to the email address of
+// the user who booked into the event
+
+
+exports.sendNotification = function(emailAddress, callback) {
+  // send notification to users email address
+}
