3766be321cc07e2b15aeb6c9aa77b544d11222a2
Richard Taylor <richardta@kainos.com>
2017-08-30 09:47:25 +0100

    Created example test that always passes

--- server/test/dbTest.js
+++ server/test/dbTest.js
@@ -1,1 +1,10 @@
 // Lets put tests here!
+
+var assert = require('assert');
+describe('db.js', function() {
+  describe('getCoursesRoute', function() {
+    it('Shoud Return Rows Object', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
